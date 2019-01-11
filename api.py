4944e6c813fb1984a1a176a93f2940c100c055c2
clayton <clayton.beraldi@jacotei.com.br>
2019-01-11 16:13:55 -0200

    inserindo template index

--- api.py
+++ api.py
@@ -1,12 +1,21 @@
-from bottle import jinja2_view, route, run, request
+import jinja2
+from bottle import route, run, request
+from jinja2 import *
 from sqlalchemy import select
 from table import table_types_of_trucks
 import json
 
-@route('/<cadastro>')
-@jinja2_view('views/cadastro.html')
-def cadastro(cadastro):
-    return dict(nome=cadastro)
+def get_template(template):
+    templateLoader = jinja2.FileSystemLoader(searchpath="./views")
+    templateEnv    = jinja2.Environment(loader=templateLoader)
+    template       = templateEnv.get_template(template)
+    return template
+
+@route('/')
+def index():
+    template = 'index.html'
+    template = get_template(template)
+    return template.render()
 
 @route('/alltypesoftrucks')
 def getAll():
@@ -18,4 +27,4 @@ def getAll():
     
     return json.dumps([dict(r) for r in res])
 
-run(port=8080)
+run(port=8082)
