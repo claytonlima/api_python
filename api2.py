e0a71c73497674ae53b46844dc0e031d8f682ba2
Clayton Beraldi <limaberaldi@gmail.com>
2019-01-14 01:06:27 -0200

    ajustando retorno do json usando sqllite

---
+++ api2.py
@@ -0,0 +1,36 @@
+import jinja2
+from bottle import route, run
+from truck_types import Session, TrucksTypes
+from sqlalchemy import text
+import json
+from stix2.base import STIXJSONEncoder
+
+def get_template(template):
+    templateLoader = jinja2.FileSystemLoader(searchpath="./views")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    template    = templateEnv.get_template(template)
+    return template.render()
+
+@route('/')
+def index():
+    template = 'index.html'
+    return get_template(template)
+
+
+@route('/alltypesoftrucks')
+def getAll():
+    session = Session()
+    stmt = text('SELECT truck_id, truck_name FROM trucks_types')
+    stmt = stmt.columns(TrucksTypes.truck_id, TrucksTypes.truck_name)
+    trucks_types = session.query(TrucksTypes).from_statement(stmt).all()
+
+    trucks = []
+    for truck_type in trucks_types:
+      trucks.append({
+          'truck_id': truck_type.truck_id,
+          'truck_name': truck_type.truck_name
+      })
+
+    return {'results': trucks}
+
+run(port=8080)
---
+++ truck_types.py
@@ -0,0 +1,40 @@
+from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import *
+
+Base = declarative_base()
+
+class TrucksTypes(Base):
+    __tablename__ = "trucks_types"
+
+    truck_id = Column('truck_id', Integer, primary_key=True)
+    truck_name = Column('truck_name', String, unique=True)
+    created_data = Column('created_data', TIMESTAMP)
+    updated_data = Column('updated_data', TIMESTAMP)
+
+
+engine = create_engine('sqlite:///../project_trucks', echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+
+# session = Session()
+# new_truck_type = TrucksTypes()
+# new_truck_type.truck_id = 9
+# new_truck_type.truck_name = 'Optimus Prime'
+# new_truck_type.created_data = datetime.now()
+# new_truck_type.updated_data = datetime.now()
+
+# session.add(new_truck_type)
+# session.commit()
+# session.close()
+#
+# session = Session()
+# trucks_types = session.query(TrucksTypes).all()
+#
+# for truck_type in trucks_types:
+#     print(truck_type.truck_id)
+#     print(truck_type.truck_name)
+#
+# session.close()
+
