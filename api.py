fa174e1ae96bb0c180d72f84ba8c83ecbfd054f4
Clayton Beraldi <limaberaldi@gmail.com>
2019-01-20 15:55:28 -0200

    consulta api geodata

--- api.py
+++ api.py
@@ -1,15 +1,15 @@
+import requests
 from bottle import route, run, request
 from model.truck_types import add, update, delete, insert_types_of_trucks_auto, list_all_trucks
-from model.drivers import *
+from model.drivers import insert_drivers_auto, get_driver
+from config.geocoding import GEOCODING_API_KEY
 from datetime import datetime
-
 
 @route('/alltypesoftrucks')
 def getAll():
     result = list_all_trucks()
 
     return {'results': result}
-
 
 @route('/typeoftruck/add', method='POST')
 def add_new_type_of_truck():
@@ -37,9 +37,45 @@ def insert_mock_type_of_trucks():
 
     return {'result': result}
 
+
 @route('/typeoftruck/del/:id', method='DELETE')
 def remove_type_of_truck(id):
     result = delete(id)
     return {'result': result}
+
+@route('/driver/list/:id', method='GET')
+def list_driver(id):
+    result = get_driver(id)
+
+    result_origin_driver = get_origin_latitude_longitude(result.origin_latitude,  result.origin_longitude)
+    result_destiny_driver = get_origin_latitude_longitude(result.destiny_latitude, result.destiny_longitude)
+
+    return {'result':
+           [
+            {'origin': result_origin_driver,
+             'destiny': result_destiny_driver
+            }
+           ]
+        }
+
+@route('/driver/addMock', method='POST')
+def add_drivers_auto():
+    result = insert_drivers_auto()
+
+    return {'result': result}
+
+def get_origin_latitude_longitude(latitude, longitude):
+    url = "https://api.opencagedata.com/geocode/v1/json?q=" + latitude + "+" + longitude + "&key=" + GEOCODING_API_KEY + "&language=pt&pretty=1"
+    request_geo_data = requests.get(url).json()
+
+    if request_geo_data['status']['code'] == 200:
+        return request_geo_data['results'][0]['formatted']
+
+def get_destiny_latitude_longitude(latitude, longitude):
+    url = "https://api.opencagedata.com/geocode/v1/json?q=" + latitude + "+" + longitude + "&key=" + GEOCODING_API_KEY + "&language=pt&pretty=1"
+    request_geo_data = requests.get(url).json()
+
+    if request_geo_data['status']['code'] == 200:
+        return request_geo_data['results'][0]['formatted']
 
 run(port=8081)
--- model/drivers.py
+++ model/drivers.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP, CHAR, text, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, CHAR, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config.db import SQLALCHEMY_DATABASE_URI
@@ -6,7 +6,7 @@ from datetime import datetime
 
 Base = declarative_base()
 
-class drivers(Base):
+class Drivers(Base):
     __tablename__ = 'drivers'
 
     driver_id = Column('driver_id', Integer, primary_key=True)
@@ -17,12 +17,41 @@ class drivers(Base):
     type_driver_license = Column('type_driver_license', CHAR)
     vehicle_loaded = Column('vehicle_loaded', CHAR)
     type_vehicle = Column('type_vehicle', Integer)
+    origin_latitude = Column('origin_latitude', String)
+    origin_longitude = Column('origin_longitude', String)
+    destiny_latitude = Column('destiny_latitude', String)
+    destiny_longitude = Column('destiny_longitude', String)
     created_data = Column('created_data', DateTime, default=datetime.now())
     updated_data = Column('updated_data', DateTime, default=datetime.now(),  onupdate=datetime.now())
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
+
+
+def insert_drivers_auto():
+    session = Session()
+
+    objects = [
+        Drivers(driver_id=1, name='João Silva', age=25, sex='M', own_vehicle='S', type_driver_license='C', vehicle_loaded='S', type_vehicle=1, origin_latitude='-23.4585103', origin_longitude='-46.7668096', destiny_latitude='-23.7306369', destiny_longitude='-46.546628', created_data=datetime.now()),
+        Drivers(driver_id=2, name='Pedro Cavalcante', age=27, sex='M', own_vehicle='S', type_driver_license='E', vehicle_loaded='N', type_vehicle=3, origin_latitude='-23.4585103', origin_longitude='-46.7668096', destiny_latitude='-23.7306369', destiny_longitude = '-46.546628', created_data=datetime.now()),
+        Drivers(driver_id=3, name='Paulo Frias', age=30, sex='M', own_vehicle='S', type_driver_license='C', vehicle_loaded='S', type_vehicle=2, origin_latitude='-23.4585103', origin_longitude='-46.7668096', destiny_latitude='-23.7306369', destiny_longitude='-46.546628', created_data=datetime.now()),
+        Drivers(driver_id=4, name='Judas Mariano', age=69, sex='M', own_vehicle='N', type_driver_license='E', vehicle_loaded='N', type_vehicle=5, origin_latitude='-23.4585103', origin_longitude='-46.7668096', destiny_latitude='-23.7306369', destiny_longitude='-46.546628', created_data=datetime.now()),
+        Drivers(driver_id=5, name='Bezerra da Silva', age=50, sex='M', own_vehicle='S', type_driver_license='C', vehicle_loaded='S', type_vehicle=4, origin_latitude='-23.4585103', origin_longitude='-46.7668096', destiny_latitude='-23.7306369', destiny_longitude='-46.546628', created_data=datetime.now()),
+     ]
+
+    result = session.bulk_save_objects(objects)
+    session.commit()
+    session.close()
+
+    return result
+
+def get_driver(id):
+    session = Session()
+    result = session.query(Drivers).get(id)
+    session.close()
+
+    return result
 
 
 
